73cf84e33003c6d5ef2bafc8a75d4e9f0fd8df90
Marina8989 <marina.mill209@gmail.com>
2020-08-18 19:55:21 -0700

    js practice

---
+++ main.js
@@ -0,0 +1,8 @@
+const revArray = [
+    {
+        id: 1,
+        name: 'John Smith',
+        job: 'UX Design',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi aliquid repellendus laborum voluptatum molestias libero itaque autem dolores id nesciunt.'
+    }
+]
